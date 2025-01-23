357ab4e112221a6da050230beafdf447b5f794f2
Thiago Tavares <Thiagoff2019@hotmail.com>
2025-01-23 15:17:06 -0300

    First Commit

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using FinSharkProjeto.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace FinSharkProjeto.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions options) : base(options)
+    {
+        
+    }
+    public DbSet<Stock> Stocks { get; set; }
+    public DbSet<Comment> Comments { get; set; }
+}
---
+++ Dtos/Stock/StockDTO.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using FinSharkProjeto.Model;
+
+namespace FinSharkProjeto.Dtos.Stock;
+
+public class StockDTO
+{
+  
+        public int Id { get; set; }
+        public string Symbol { get; set; } = string.Empty;
+        public string CompanyName { get; set; } = string.Empty;
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Purchase { get; set; }
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal LastDiv { get; set; }
+        public string Industry { get; set; } = string.Empty;
+        public long MarketCap { get; set; }
+
+}
